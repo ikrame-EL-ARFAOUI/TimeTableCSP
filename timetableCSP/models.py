7a63255c87e5c76a192c07c850c1e97a6cab1b32
ik <ikrameelarfaoui@gmail.com>
2021-06-17 14:12:01 +0100

    algorithme + using database data instead of the file data

--- timetableCSP/models.py
+++ timetableCSP/models.py
@@ -12,7 +12,7 @@ class Room(models.Model):
     room_name = models.CharField(max_length=2000, null=True)
     room_max_students = models.IntegerField(null=True)
     def __str__(self):
-        return self.room_id #+ ' - ' + self.room_name
+        return self.room_id + ' - ' + self.room_name
 
 
 class Teacher(models.Model):
@@ -23,11 +23,12 @@ class Teacher(models.Model):
 
 
 class Subject(models.Model):
+
     subject_id = models.CharField(max_length=1000, primary_key=True)
     subject_name = models.CharField(max_length=1000, null=True)
     subject_number_of_students = models.IntegerField(null=True)
     subject_groups = models.IntegerField(null=True)
-    subject_teacher = models.ForeignKey(Teacher,null=True,on_delete=models.SET_NULL)
+    teacher = models.ForeignKey(Teacher,null=True,on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.subject_id + ' - ' + self.subject_name
@@ -36,7 +37,7 @@ class Speciality(models.Model):
 
     speciality_id = models.CharField(max_length=2000, primary_key=True)
     speciality_name = models.CharField(max_length=2000, null=True)
-    speciality_subjects = models.ManyToManyField(Subject)
+    subjects = models.ManyToManyField(Subject, null=True)
 
     def __str__(self):
         return self.speciality_id
